f1bcf52ef375bb743936701cf7da171a8318d8db
Harshikesh <harshibabu604@gmail.com>
2024-06-19 03:46:11 +0530

    graph:removeVertex method added.

--- datastructures/src/datastructures/graph/Graph.java
+++ datastructures/src/datastructures/graph/Graph.java
@@ -27,4 +27,26 @@ public class Graph {
 		System.out.println("any one of vertex is not present!");
 		return false;
 	}
+
+	public boolean removeEdge(String string, String string2) {
+		if(adjList.containsKey(string) && adjList.containsKey(string2)) {
+			adjList.get(string2).remove(string);
+			adjList.get(string).remove(string2);
+			return true;
+		}
+		System.out.println("any one of vertex is not present!");
+		return false;
+		
+	}
+
+	public boolean removeVertex(String vertex) {
+		if(adjList.containsKey(vertex)) {
+			adjList.get(vertex).forEach(eachVertex->{
+				adjList.get(eachVertex).remove(vertex);
+			});
+			adjList.remove(vertex);
+			return true;
+		}
+		return false;
+	}
 }
--- datastructures/src/datastructures/graph/Main.java
+++ datastructures/src/datastructures/graph/Main.java
@@ -19,33 +19,96 @@ public class Main {
 //
 //	        */
 		
-		 Graph myGraph = new Graph();
+//		 Graph myGraph = new Graph();
+//
+//	        myGraph.addVertex("A");
+//	        myGraph.addVertex("B");
+//
+//
+//	        System.out.println("\nGraph before addEdge():");
+//	        myGraph.printGraph();
+//
+//
+//	        myGraph.addEdge("A", "B");
+//
+//
+//	        System.out.println("\nGraph after addEdge():");
+//	        myGraph.printGraph();
+//
+//
+//	        /*
+//	            EXPECTED OUTPUT:
+//	            ----------------
+//	            Graph before addEdge():
+//	            {A=[], B=[]}
+//	            
+//	            Graph after addEdge():
+//	            {A=[B], B=[A]}
+//
+//	        */
+		
+//		 Graph myGraph = new Graph();
+//
+//	        myGraph.addVertex("A");
+//	        myGraph.addVertex("B");
+//	        myGraph.addVertex("C");
+//
+//	        myGraph.addEdge("A", "B");
+//	        myGraph.addEdge("A", "C");
+//	        myGraph.addEdge("B", "C");
+//
+//
+//	        System.out.println("\nGraph before removeEdge():");
+//	        myGraph.printGraph();
+//
+//	        myGraph.removeEdge("A", "B");
+//
+//	        System.out.println("\nGraph after removeEdge():");
+//	        myGraph.printGraph();
+//
+//	        /*
+//	            EXPECTED OUTPUT:
+//	            ----------------
+//	            Graph before removeEdge():
+//	            {A=[B, C], B=[A, C], C=[A, B]}
+//	            
+//	            Graph after removeEdge():
+//	            {A=[C], B=[C], C=[A, B]}
+//
+//	        */
+		
+		Graph myGraph = new Graph();
 
-	        myGraph.addVertex("A");
-	        myGraph.addVertex("B");
+        myGraph.addVertex("A");
+        myGraph.addVertex("B");
+        myGraph.addVertex("C");
+        myGraph.addVertex("D");
+
+        myGraph.addEdge("A", "B");
+        myGraph.addEdge("A", "C");
+        myGraph.addEdge("A", "D");
+        myGraph.addEdge("B", "D");
+        myGraph.addEdge("C", "D");
 
 
-	        System.out.println("\nGraph before addEdge():");
-	        myGraph.printGraph();
+        System.out.println("\nGraph before removeVertex():");
+        myGraph.printGraph();
 
+        myGraph.removeVertex("D");
 
-	        myGraph.addEdge("A", "B");
+        System.out.println("\nGraph after removeVertex():");
+        myGraph.printGraph();
+        
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            Graph before removeVertex():
+            {A=[B, C, D], B=[A, D], C=[A, D], D=[A, B, C]}
+            
+            Graph after removeVertex():
+            {A=[B, C], B=[A], C=[A]}
 
-
-	        System.out.println("\nGraph after addEdge():");
-	        myGraph.printGraph();
-
-
-	        /*
-	            EXPECTED OUTPUT:
-	            ----------------
-	            Graph before addEdge():
-	            {A=[], B=[]}
-	            
-	            Graph after addEdge():
-	            {A=[B], B=[A]}
-
-	        */
+        */
 
 	}
 
