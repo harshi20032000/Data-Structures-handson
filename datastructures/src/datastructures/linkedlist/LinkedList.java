c12dd19b841f3e88bb2162892a3352c6fb71575e
Harshikesh <harshibabu604@gmail.com>
2024-07-16 01:08:18 +0530

    removeFirst LL

--- datastructures/src/datastructures/linkedlist/LinkedList.java
+++ datastructures/src/datastructures/linkedlist/LinkedList.java
@@ -2,102 +2,117 @@ package datastructures.linkedlist;
 
 public class LinkedList {
 	private Node head;
-    private Node tail;
-    private int length;
+	private Node tail;
+	private int length;
 
-    class Node {
-        int value;
-        Node next;
+	class Node {
+		int value;
+		Node next;
 
-        Node(int value) {
-            this.value = value;
-        }
-    }
+		Node(int value) {
+			this.value = value;
+		}
+	}
 
-    public LinkedList(int value) {
-        Node newNode = new Node(value);
-        head = newNode;
-        tail = newNode;
-        length = 1;
-    }
+	public LinkedList(int value) {
+		Node newNode = new Node(value);
+		head = newNode;
+		tail = newNode;
+		length = 1;
+	}
 
-    public void printList() {
-        Node temp = head;
-        while (temp != null) {
-            System.out.println(temp.value);
-            temp = temp.next;
-        }
-    }
+	public void printList() {
+		Node temp = head;
+		while (temp != null) {
+			System.out.println(temp.value);
+			temp = temp.next;
+		}
+	}
 
-    public void getHead() {
-        if (head == null) {
-            System.out.println("Head: null");
-        } else {
-            System.out.println("Head: " + head.value);
-        }
-    }
+	public void getHead() {
+		if (head == null) {
+			System.out.println("Head: null");
+		} else {
+			System.out.println("Head: " + head.value);
+		}
+	}
 
-    public void getTail() {
-        if (head == null) {
-            System.out.println("Tail: null");
-        } else {
-            System.out.println("Tail: " + tail.value);
-        }
-    }
-    
-    public void makeEmpty() {
-    	head=null;
-    	tail=null;
-    	length=0;
-    }
+	public void getTail() {
+		if (head == null) {
+			System.out.println("Tail: null");
+		} else {
+			System.out.println("Tail: " + tail.value);
+		}
+	}
 
-    public void getLength() {
-        System.out.println("Length: " + length);
-    }
-    
-    public void append(int value) {
-        Node newNode = new Node(value);
-        if (length == 0) {
-            head = newNode;
-            tail = newNode;
-        } else {
-            tail.next = newNode;
-            tail = newNode;
-        }
-        length++;
-    }
-    
-    public Node removeLast() {
-    	if(length==0) return null;
-    	Node temp=head;
-    	Node pre =head;
-    	while(temp.next!=null) {
-    		pre=temp;
-    		temp=temp.next;
-    	}
-    	length--;
-    	pre.next=null;
-    	tail=pre;
+	public void makeEmpty() {
+		head = null;
+		tail = null;
+		length = 0;
+	}
+
+	public void getLength() {
+		System.out.println("Length: " + length);
+	}
+
+	public void append(int value) {
+		Node newNode = new Node(value);
+		if (length == 0) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			tail.next = newNode;
+			tail = newNode;
+		}
+		length++;
+	}
+
+	public Node removeLast() {
+		if (length == 0)
+			return null;
+		Node temp = head;
+		Node pre = head;
+		while (temp.next != null) {
+			pre = temp;
+			temp = temp.next;
+		}
+		length--;
+		pre.next = null;
+		tail = pre;
 //    	need to handle one edge case, if length is 1 then head and tail must be pointed to null after this operation
-    	if(length==0) {
-    		tail=null;
-    		head=null;
-    	}
-    	
-    	return temp;
-    	
-    }
+		if (length == 0) {
+			tail = null;
+			head = null;
+		}
+
+		return temp;
+
+	}
 
 	public void prepend(int value) {
 		Node newNode = new Node(value);
-        if (length == 0) {
-            head = newNode;
-            tail = newNode;
-        } else {
-           newNode.next=head;
-           head=newNode;
-        }
-        length++;
+		if (length == 0) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			newNode.next = head;
+			head = newNode;
+		}
+		length++;
+	}
+
+	public Node removeFirst() {
+		if (length == 0)
+			return null;
+		Node temp = head;
+		head = head.next;
+		temp.next = null;
+		length--;
+		if (length == 0) {
+			tail = null;
+			head = null;
+		}
+		return temp;
 	}
 
 }
