061fb51a41257f59c81b7d72053f2423b73945ba
Harshikesh <harshibabu604@gmail.com>
2024-07-18 02:44:05 +0530

    added remove method LL

--- datastructures/src/datastructures/linkedlist/LinkedList.java
+++ datastructures/src/datastructures/linkedlist/LinkedList.java
@@ -156,4 +156,21 @@ public class LinkedList {
 		
 	}
 
+	public Node remove(int index) {
+		if (index < 0 || index > length)
+			return null;
+		if (index == 0) {
+			return removeFirst();
+		}
+		if (index == length) {
+			return removeLast();
+		}
+		Node prev = get(index-1);
+		
+		Node temp=prev.next;
+		prev.next=prev.next.next;
+		length--;
+		return temp;
+	}
+
 }
