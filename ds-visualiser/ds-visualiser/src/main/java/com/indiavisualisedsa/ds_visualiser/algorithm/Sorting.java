1c248b549ff2b9b046cc5c4feaac116a337285fa
Harshikesh <harshibabu604@gmail.com>
2024-11-16 14:36:07 +0530

    mergeSort added.

--- ds-visualiser/ds-visualiser/src/main/java/com/indiavisualisedsa/ds_visualiser/algorithm/Sorting.java
+++ ds-visualiser/ds-visualiser/src/main/java/com/indiavisualisedsa/ds_visualiser/algorithm/Sorting.java
@@ -57,6 +57,54 @@ public class Sorting {
 		}
 		
 	} 
+	
+	/**merges two sorted arrays
+	 * 
+	 * @param array1
+	 * @param array2
+	 * @return merged value of two sorted arrays
+	 */
+	static int[] merge(int[] array1, int[] array2) {
+		int[] combined = new int[array1.length+array2.length];
+		int index=0;
+		int i=0;
+		int j=0;
+		while(i<array1.length && j<array2.length) {
+			if(array1[i]<array2[j]) {
+				combined[index]=array1[i];
+				index++;
+				i++;
+			}
+			else {
+				combined[index]=array2[j];
+				index++;
+				j++;
+			}
+		}
+		while(i<array1.length) {
+			combined[index]=array1[i];
+			index++;
+			i++;
+		}
+		while(j<array2.length) {
+			combined[index]=array2[j];
+			index++;
+			j++;
+		}
+		return combined;
+	}
+	
+	/**Testing merge sort on unsorted array
+	 * Time complexity - nlogn
+	 * @param array
+	 * @return sorted array.
+	 */
+	static int[] mergeSort(int[] array) {
+		if(array.length==1) return array;
+		int[] left = mergeSort(Arrays.copyOfRange(array, 0, array.length/2));
+		int[] right = mergeSort(Arrays.copyOfRange(array,array.length/2, array.length));
+		return merge(left, right);
+	}
 
 	public static void main(String[] args) {
 		int[] array = { 7, 9, 8, 0, 1, 4 };
@@ -71,5 +119,12 @@ public class Sorting {
 		System.out.println("Sorting insertionSort");
 		selectionSort(array2);
 		System.out.println(Arrays.toString(array2));
+		int[] array3 = {1, 3, 5, 7, 9, 11, 13, 15};
+		int[] array4 = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+		System.out.println("Testing merge on two sorted arrays");
+		System.out.println(Arrays.toString(merge(array3, array4)));
+		int[] array5 = {1, 3, 5, 7, 9, 11, 13, 15, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+		System.out.println("Testing merge sort");
+		System.out.println(Arrays.toString(mergeSort(array5)));
 	}
 }
